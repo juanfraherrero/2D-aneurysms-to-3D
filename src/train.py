342c0337da75c3be0f6c6c5ed6c005f98df6a211
Juan Francisco Herrero <juanfraherrero@gmail.com>
2024-06-01 19:54:33 -0300

    Add posible to profile by arg or by config to check slower parts of code, have to add the profiling wrappers over code to profile

--- src/train.py
+++ src/train.py
@@ -1,8 +1,9 @@
 import os
 import torch
-from torch import optim
+import torch.autograd.profiler as profiler
 from models.Model import CombinedImagesAutoencoder
 from utils import utils
+from utils.warmUpModel import warmUpModel 
 from arguments.arguments import parseTrainArguments
 from train.train import train
 
@@ -22,8 +23,6 @@ if __name__ == '__main__':
     
     # Create model
     model = CombinedImagesAutoencoder()
-    if(useCuda):
-        model = model.to('cuda')
 
     # create folder to save info of training
     folder_path, models_path, results_path, charts_path = utils.get_folder_for_training_model(args.folder_result, config)
@@ -37,8 +36,18 @@ if __name__ == '__main__':
     # get if running in colab
     isRunningInColab = utils.getColab(config, args.colab)
 
+    # get learning rate
+    resume_training = utils.getResumeEpoch(config, args.resume_training)
+    
+    # get checkpoint path
+    checkpoint_path = utils.getCheckpointPath(config, args.checkpoint_path)
+    
+    # Profiling
+    profiling = utils.getProfiling(config, args.profiling)
+
     # Save info model in txt before training  
     model_info_path = os.path.join(models_path,'model_info.txt')
+
     with open(model_info_path, 'w') as file:
         file.write("INFO OF MODEL:\n")
         file.write(str(model))
@@ -49,11 +58,24 @@ if __name__ == '__main__':
         file.write("Image size: "+ str(loaders_info["image_size"])+"\n")
         file.write("Learning rate: "+str(learning_rate)+"\n")
         file.write("Epochs: "+ str(epochs) +"\n")
+
+    if profiling:
+        if(useCuda):
+            model.to('cuda')
         
-        
+        input = torch.rand(128, 500).cuda()
+        mask = torch.rand((500, 500, 500), dtype=torch.float).cuda()
+
+        # warm up
+        warmUpModel(model, train_loader)
+
+        with profiler.profile(record_shapes=True) as prof:
+            train(model, train_loader, eval_loader, epochs, models_path, charts_path, learning_rate, isRunningInColab, resume_training, checkpoint_path, useCuda)
+        print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
+        exit()
 
     # Train the model
-    train(model, train_loader, eval_loader, epochs=epochs, models_path=models_path, charts_path=charts_path, learning_rate=learning_rate, isRunningInColab=isRunningInColab)
+    train(model, train_loader, eval_loader, epochs=epochs, models_path=models_path, charts_path=charts_path, learning_rate=learning_rate, isRunningInColab=isRunningInColab, resume_training=resume_training, checkpoint_path=checkpoint_path, useCuda=useCuda)
 
     # Guardar el modelo
     torch.save(model.state_dict(), os.path.join(models_path,'model.pth'))
